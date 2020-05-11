0b0de8dcd7d1be0b7a6098c32998b6c968546dd2
Will Tackett <will.s.tackett@gmail.com>
2020-05-11 18:10:54 -0400

    0.2.12: .html.zip

--- run.py
+++ run.py
@@ -76,7 +76,9 @@ def cleanup():
     os.system("cp ../templates/fittext.js {}".format(html_dir))
     os.system("rm *.html")
     os.system("zip -r {0}/{1}_results.zip {2}".format(gear_output_dir,subject_label, subject_label + '_results' ))
-    os.system("zip -r {0}/{1}_report.zip {2}".format(gear_output_dir,subject_label, subject_label + '_report'))
+    os.chdir(html_dir)
+    os.system("zip -r {0}/{1}_report.html.zip {2}".format(gear_output_dir,subject_label, './*'))
+    os.chdir(html_dir)
     os.system("rm -rf heatmap_run.sh results *_results *_report")
 
 
